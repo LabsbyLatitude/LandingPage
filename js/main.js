7337e768626f23e6168e3fe2779f12de146d3fe3
Christopher Obiozor <chris@chrisobiozor.com>
2022-08-13 21:44:12 -0400

    Reimplement landing page
    - fixed top navbar
    - hero section with fixed attachement background image
    - solutions section with responsive feature groups via
      bootstrap grid and other utilities

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function (){
+
+    // offset the header top by the navbar height
+    
+    // let nav_height = $(".navbar").outerHeight();
+    // alert(nav_height);
+    // $("header").css("top", nav_height);
+
+
+    // use jQuery to scroll to selected section
+    $(".navbar a").click(function () {
+        // console.log("#" + $(this).data("value"));
+        const section_offset = $("#" + $(this).data("value")).offset().top;
+        const nav_height = $(".navbar").outerHeight();
+
+        $("body,html").animate(
+            {
+                scrollTop: section_offset-nav_height,
+            },
+            1000
+        );
+    });
+
+});
