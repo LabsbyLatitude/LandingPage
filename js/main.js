72d8ae620dec169159ee3f07d8159ec28e5494b9
Christopher Obiozor <chris@chrisobiozor.com>
2023-01-08 06:33:08 -0500

    Add play videon on scroll and/or hover CU-39j7g99[CLOSED]

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-$(document).ready(function (){
+// add a custom playing property to video elements
+// https://stackoverflow.com/questions/6877403/how-to-tell-if-a-video-element-is-currently-playing
+Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+    get: function(){
+        // return !!(this.currentTime >= 0 && !this.paused && !this.ended && this.readyState > 2);
+        return !!(!this.paused);
+    }
+})
 
+$(document).ready(function () {
+    
     // offset the header top by the navbar height
     
     // let nav_height = $(".navbar").outerHeight();
@@ -15,12 +24,82 @@ $(document).ready(function (){
 
         $("body,html").animate(
             {
-                scrollTop: section_offset-nav_height,
+                scrollTop: section_offset - nav_height,
             },
             1000
         );
     });
 
     $(".carousel-item").attr("data-bs-interval", 5000);
+
+
+    // Play/Pause feature videos when within a middle horizontal band/region of the
+    // screen whenever the window scrolls
+    $(window).scroll(function () {
+        // get all feature videos
+        // let videos = document.getElementsByClassName("f-vid");
+        // loop over each and check location on screen
+        // for(let video of videos){ console.log(video.id); }
+
+        $(".f-vid video").each(playVideoHandler);
+    });
+
+    // play video when hovered over
+    $(".f-vid video").mouseenter(function (event) {
+        // change box shadow color
+        // $(this).animate({
+        //     "box-shadow": "0px 0px 15px 1px rgba(116, 28, 135, .6)"
+        // }, 100);
+        $(this).css("box-shadow", "0px 0px 15px 1px rgba(116, 28, 135, .6)");
+
+        // if (!this.playing)
+        this.play();
+    });
+    
+    // pause video on mouse leave if it's outside of the viewport play region
+    $(".f-vid video").mouseleave(playVideoHandler);
+
+    /**
+     * Play or pause the video html element this function is called in the context
+     * of depending on whether the it's within a center region of the viewport.
+     */
+    function playVideoHandler() {
+        let viewportHeight = window.innerHeight; // height of window in pixels
+        let scrollTop = $(document).scrollTop(); // get scroll position of window
+
+        // define viewport play range. include offset for navbar.
+        let topLimit = 0 + $(".navbar").outerHeight();
+        let topLimit2 = viewportHeight / 3 + $(".navbar").outerHeight();
+        let bottomLimit = viewportHeight * 3 / 4;
+        let bottomLimit2 = viewportHeight * 2 / 3;
+
+        let boundingRect = this.getBoundingClientRect();
+        let videoTop = boundingRect.top; // top edge of video
+        let videoCenter = boundingRect.top + boundingRect.height / 2; // center of video
+
+        // play if video is within center region of screen
+
+        // if(videoCenter >= topLimit2 && videoCenter < bottomLimit2){ 
+        if (videoTop >= topLimit && videoTop < bottomLimit) {
+            // change box shadow color
+            // $(this).animate({
+            //     "box-shadow": "0px 0px 15px 1px rgba(116, 28, 135, .6)"
+            // }, 100);
+            $(this).css("box-shadow", "0px 0px 15px 1px rgba(116, 28, 135, .6)");
+
+            this.play();
+        }
+        else {
+            // reset box-shadow to original value defined in css file
+            // $(this).animate({
+            //     box-shadow: "" 
+            // }, 100);
+            $(this).css("box-shadow", "");
+            
+            this.pause();
+        }
+
+        // console.log(`mouseleave: video [${this.id}] top: ${videoTop} top/vh: ${videoTop/viewportHeight}`);
+    }
 
 });
